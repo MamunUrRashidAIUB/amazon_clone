0c59b9e39a913b44a74db19592b2b2c51b2cd3eb
USER <USER@DESKTOP-TV8AAGO>
2024-02-01 18:42:39 +0600

    create accounte created

--- lib/features/auth/screen/authscreen.dart
+++ lib/features/auth/screen/authscreen.dart
@@ -1,7 +1,13 @@
+import "package:amazon_clone/constants/globalvariables.dart";
 import "package:flutter/material.dart";
 
+enum auth {
+  singin,
+  signup;
+}
+
 class Authscreen extends StatefulWidget {
-  static const String routename='/auth-screen';
+  static const String routename = '/auth-screen';
   const Authscreen({super.key});
 
   @override
@@ -9,10 +15,39 @@ class Authscreen extends StatefulWidget {
 }
 
 class _AuthscreenState extends State<Authscreen> {
+  auth _auth =auth.signup;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: GlobalVariables.greyBackgroundCOlor,
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              "welcome",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+            ),
+            ListTile(
+              title: Text(
+                "Create account.",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              leading: Radio(
+                activeColor: GlobalVariables.secondaryColor,
+                value: auth.signup,
+                groupValue:_auth ,
+                onChanged: (auth? val){
+                  setState(() {
+                    _auth=val!;
+                  });
+                },
+              ),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
